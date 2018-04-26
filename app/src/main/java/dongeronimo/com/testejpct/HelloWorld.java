f3f7e9d8e865822ad6057c84bd4646b36e1d7104
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-04-25 22:50:37 -0300

    Está mostrando minha primeira geometria: um plano com uma textura qqer.

--- app/src/main/java/dongeronimo/com/testejpct/HelloWorld.java
+++ app/src/main/java/dongeronimo/com/testejpct/HelloWorld.java
@@ -1,18 +1,44 @@
 package dongeronimo.com.testejpct;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-
+/**
+ * Essa tela mostra a cena 3d. No momento seu layout é construído manualmente, ignorando o arquivo
+ * de layout.
+ * */
 public class HelloWorld extends Activity {
-
+    private GLSurfaceView glView;
+    private MyRenderer renderer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Invoca o construtor herdado antes de fazer as coisas do filho, necessário
         super.onCreate(savedInstanceState);
         //Cria a surface view
+        glView = new GLSurfaceView(getApplication());
+        glView.setEGLContextClientVersion(2);
+        glView.setPreserveEGLContextOnPause(true);
         //Cria o renderer e o põe na surface view
+        renderer = new MyRenderer(getApplication());
+        glView.setRenderer(renderer);
         //Põe a surface view na tela
-
+        setContentView(glView);
         //setContentView(R.layout.activity_hello_world);//não vai ser usado nesse momento.
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        glView.onPause();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        glView.onResume();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        System.exit(0);
     }
 }
---
+++ app/src/main/java/dongeronimo/com/testejpct/MyRenderer.java
@@ -0,0 +1,98 @@
+package dongeronimo.com.testejpct;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import com.threed.jpct.Camera;
+import com.threed.jpct.FrameBuffer;
+import com.threed.jpct.Light;
+import com.threed.jpct.Object3D;
+import com.threed.jpct.Primitives;
+import com.threed.jpct.RGBColor;
+import com.threed.jpct.SimpleVector;
+import com.threed.jpct.Texture;
+import com.threed.jpct.TextureManager;
+import com.threed.jpct.World;
+import com.threed.jpct.util.MemoryHelper;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static com.threed.jpct.util.BitmapHelper.convert;
+import static com.threed.jpct.util.BitmapHelper.rescale;
+
+/**
+ * Implementação do renderer. É usado pela HelloWorld (no glView que tem lá)*/
+public class MyRenderer implements GLSurfaceView.Renderer {
+    private GL10 lastGl = null;
+    private FrameBuffer fb = null;
+    private World world = null;
+    private Light sun = null;
+    private Texture texture = null;
+    private Object3D obj = null;
+    private Context context;
+
+    public MyRenderer(Context ctx){
+        context = ctx;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int w, int h) {
+        //Atualização/criação do framebuffer
+        if(lastGl!=gl10){//Cria o framebuffer
+            Log.i("Hello world", "init buffer");
+            if(fb!=null){
+                fb.dispose();
+            }
+            fb = new FrameBuffer(w, h);//Cria o framebuffer
+            fb.setVirtualDimensions(fb.getWidth(), fb.getHeight());
+            lastGl = gl10;
+        }else{//Não precisa recriar o framebuffer, só redimensionar
+            fb.resize(w,h);
+            fb.setVirtualDimensions(w,h);
+        }
+        //criação do mundo se ele não tiver sido criado
+        if(world==null){
+            world = new World();
+            world.setAmbientLight(20,20,20);
+            sun = new Light(world);
+            sun.setIntensity(250,250,250);
+            //cria a textura
+            texture = new Texture(rescale(convert(context.getResources().getDrawable(R.drawable.imagem)), 256,256));
+            TextureManager.getInstance().addTexture("texture", texture);
+            //criação o objeto (um plano)
+            obj = Primitives.getPlane(1, 20f);
+            world.addObject(obj);
+            obj.translate(0, 0, 0);
+            obj.setTexture("texture");
+            obj.build();
+
+            Camera cam = world.getCamera();
+            cam.moveCamera(Camera.CAMERA_MOVEOUT, 15);
+            cam.lookAt(SimpleVector.ORIGIN);
+
+            SimpleVector sv = new SimpleVector();
+            sv.set(SimpleVector.ORIGIN);
+            sv.y -= 100;
+            sv.z -= 100;
+            sun.setPosition(sv);
+            MemoryHelper.compact();
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        // Draw the main screen
+        fb.clear(RGBColor.GREEN);
+        world.renderScene(fb);
+        world.draw(fb);
+        fb.display();
+
+    }
+}
