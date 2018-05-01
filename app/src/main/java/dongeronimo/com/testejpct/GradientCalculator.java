69753f369d4cd6cd6343cb537f8b05dbb8feddef
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-05-01 12:34:56 -0300

    Feita a classe de cálculo dos gradientes.

--- app/src/main/java/dongeronimo/com/testejpct/GradientCalculator.java
+++ app/src/main/java/dongeronimo/com/testejpct/GradientCalculator.java
@@ -1,7 +1,5 @@
 package dongeronimo.com.testejpct;
 
-import android.util.Log;
-
 /**
  * https://en.wikipedia.org/wiki/Image_gradient
  * Encapsula o cálculo do gradiente e da magnitude do gradiente do mapa. Os cálculos são feitos no
@@ -12,34 +10,60 @@ import android.util.Log;
 public class GradientCalculator {
     private float data[][];
     //O valor dos componentes horizontais do gradiente (dIdX)
-    private float gX[][];
+    private float g_x[][];
     //o valor dos componentes verticais do gradiente(dIdY);
-    private float gY[][];
+    private float g_y[][];
+    //A magnitude do gradiente
+    private float mag[][];
+    //A direção do gradiente
+    private float direction[][];
     public GradientCalculator(float data[][]){
         this.data = data;
-        gX = new float[data.length][data[0].length];
-        gY = new float[data.length][data[0].length];
+        g_x = new float[data.length][data[0].length];
+        g_y = new float[data.length][data[0].length];
+        mag = new float[data.length][data[0].length];
+        direction = new float[data.length][data[0].length];
         //cálculo do gradiente horizontal
         for(int ln=0; ln<data.length; ln++){
             for(int col = 0; col < data[ln].length; col++){
-                float f0 = col==0 ? data[ln][col] : data[ln][col-1];//isso aqui é pra lidar com a borda esquerda
-                float f1 = data[ln][col];
-                float f2 = col ==(data[ln].length-1)? data[ln][col]: data[ln][col+1];//isso aqui é pra lidar com a borda direita
-                float dIdX = f0 * -1.0f + f1 * 0.0f + f2 * 1.0f;
-                gX[ln][col] = dIdX;
+                //Cálculo do gradiente horizontal
+                float _x0 = col==0 ? data[ln][col] : data[ln][col-1];//isso aqui é pra lidar com a borda esquerda
+                float _x = data[ln][col];
+                float _x1 = col ==(data[ln].length-1)? data[ln][col]: data[ln][col+1];//isso aqui é pra lidar com a borda direita
+                float dIdX = _x0 * -1.0f + _x * 0.0f + _x1 * 1.0f;
+                g_x[ln][col] = dIdX;
+                //cálculo do gradiente vertical
+                float _y0 = ln==0 ? data[ln][col] : data[ln-1][col];
+                float _y = data[ln][col];
+                float _y1 = ln == (data.length-1) ? data[ln][col]: data[ln+1][col];//isso aqui é pra lidar com a borda direita
+                float dIdY = _y0 * -1.0f + _y * 0.0f + _y1 * 1.0f;
+                g_y[ln][col] = dIdY;
+                //cálculo da magnitude do gradiente.
+                float magDxy = (float) Math.sqrt( dIdY *dIdY + dIdX* dIdX);
+                mag[ln][col] = magDxy;
+                //A direção do gradiente
+                float tetha = (float) Math.atan2(dIdY, dIdX);
+                direction[ln][col] = tetha;
             }
         }
-        //cálculo do gradiente vertical
-        for(int ln=0; ln<data.length; ln++){
-            for(int col = 0; col < data[ln].length; col++){
-                float f0 = ln==0 ? data[ln][col] : data[ln-1][col];
-                float f1 = data[ln][col];
-                float f2 = ln == (data.length-1) ? data[ln][col]: data[ln+1][col];//isso aqui é pra lidar com a borda direita
-                float dIdY = f0 * -1.0f + f1 * 0.0f + f2 * 1.0f;
-                gY[ln][col] = dIdY;
-            }
-        }
-        Log.d("aa","calc grad");
-        //cálculo da magnitude do gradiente
+    }
+    public float[][] getG_x() {
+        return g_x;
+    }
+
+    public float[][] getG_y() {
+        return g_y;
+    }
+
+    public float[][] getData() {
+        return data;
+    }
+
+    public float[][] getDirection() {
+        return direction;
+    }
+
+    public float[][] getMag() {
+        return mag;
     }
 }
