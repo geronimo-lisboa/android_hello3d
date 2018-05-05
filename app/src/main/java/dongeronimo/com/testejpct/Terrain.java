f497f646f3465e9d1af72b39cee1567b7a68305e
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-05-05 10:51:17 -0300

    Criando uma serie de mapas com tamanhos diferenciados para testar as velocidades

--- app/src/main/java/dongeronimo/com/testejpct/Terrain.java
+++ app/src/main/java/dongeronimo/com/testejpct/Terrain.java
@@ -8,7 +8,6 @@ import com.threed.jpct.GLSLShader;
 import com.threed.jpct.Loader;
 import com.threed.jpct.Object3D;
 import com.threed.jpct.SimpleVector;
-
 /**
  * O terreno gerado a partir de um heightmap. A partir de um bitmap passado pro construtor constrói
  * o terreno. O bitmap deve ser em tons de cinza porque a cor do componente de menor precedência do pixel
@@ -25,24 +24,17 @@ public class Terrain {
     private GradientCalculator gradientCalculator;
 
     private void buildTerrain(){
-        final long t0 = System.currentTimeMillis();
         //Construção da geometria
         final int bmpLargura = heightmapBitmap.getWidth();
         final int bmpAltura = heightmapBitmap.getHeight();
         final int numeroDeFaces = (bmpLargura-1) * (bmpAltura-1);
         final int numeroDeTriangulos = numeroDeFaces * 2;
-        //superficie = new Object3D(numeroDeTriangulos);
+        superficie = new Object3D(numeroDeTriangulos);
         //calculo do centro do mundo
         final float worldOffsetX =  bmpLargura/2 * -1.0f;
         final float worldOffsetZ =  bmpAltura/2 * -1.0f;
-        //As listas guardarão os dados que depois serão passados pra jpct.
-        float coords[] = new  float[numeroDeTriangulos*9];
-        float uvs[] = new float[numeroDeTriangulos*6];
-        int indexes[] = new int[numeroDeTriangulos*3];
-
-        int currentCoord = 0;
-        int currentUv = 0;
-        int currentIndex = 0;
+        long tt = 0;
+        long t0 = System.currentTimeMillis();
         //percorre o heightmap criando as faces.
         for(int y=0; y<bmpAltura-1; y++){
             for(int x=0; x<bmpLargura-1; x++){
@@ -54,84 +46,25 @@ public class Terrain {
                 SimpleVector ponto04 = new SimpleVector(x+ worldOffsetX,heightValues[x][y+1], y+1+worldOffsetZ);
                 SimpleVector ponto05 = new SimpleVector(x+1+ worldOffsetX,heightValues[x+1][y+1], y+1 +worldOffsetZ);
                 SimpleVector ponto06 = new SimpleVector(x+1+ worldOffsetX,heightValues[x+1][y], y+worldOffsetZ);
-                ////Adiciona às listas.
-                //vertice 1
-                coords[currentCoord + 0] = ponto01.x;
-                coords[currentCoord + 1] = ponto01.y;
-                coords[currentCoord + 2] = ponto01.z;
-                uvs[currentUv + 0 ]=0;
-                uvs[currentUv + 1 ]=0;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
-                //Vertice 2
-                coords[currentCoord + 0] = ponto02.x;
-                coords[currentCoord + 1] = ponto02.y;
-                coords[currentCoord + 2] = ponto02.z;
-                uvs[currentUv + 0 ]=0;
-                uvs[currentUv + 1 ]=1;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
-                //Vertice 3
-                coords[currentCoord + 0] = ponto03.x;
-                coords[currentCoord + 1] = ponto03.y;
-                coords[currentCoord + 2] = ponto03.z;
-                uvs[currentUv + 0 ]=1;
-                uvs[currentUv + 1 ]=0;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
-                //Vertice 4
-                coords[currentCoord + 0] = ponto04.x;
-                coords[currentCoord + 1] = ponto04.y;
-                coords[currentCoord + 2] = ponto04.z;
-                uvs[currentUv + 0 ]=0;
-                uvs[currentUv + 1 ]=1;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
-                //Vertice 5
-                coords[currentCoord + 0] = ponto05.x;
-                coords[currentCoord + 1] = ponto05.y;
-                coords[currentCoord + 2] = ponto05.z;
-                uvs[currentUv + 0 ]=1;
-                uvs[currentUv + 1 ]=1;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
-                //Vertice 6
-                coords[currentCoord + 0] = ponto06.x;
-                coords[currentCoord + 1] = ponto06.y;
-                coords[currentCoord + 2] = ponto06.z;
-                uvs[currentUv + 0 ]=1;
-                uvs[currentUv + 1 ]=0;
-                indexes[currentIndex]=currentIndex;
-                //avança os contadores
-                currentCoord = currentCoord+3;
-                currentUv = currentUv+2;
-                currentIndex = currentIndex+1;
+                //Adiciona à superficie
+                superficie.addTriangle(ponto01,0,0, ponto02,0,1, ponto03,1,0);
+                superficie.addTriangle(ponto04,0,1, ponto05,1,1, ponto06,1,0);
             }
         }
-        //passa as listas pro objeto
-        superficie = new Object3D(coords, uvs, indexes, 0);
+        long t1 = System.currentTimeMillis();
+        Log.d("TEMPO_PASSAGEM_GEO", (t1-t0)+" ms");
+        tt = tt + (t1-t0);
+        t0 = System.currentTimeMillis();
         superficie.setCulling(false);
         superficie.calcBoundingBox();
         superficie.calcCenter();
-        superficie.strip();
-        superficie.compile(false, true);
         superficie.build();
-
+        superficie.strip();
+        superficie.build();
+        t1 = System.currentTimeMillis();
+        Log.d("TEMPO_BUILD_DA_API", (t1-t0)+" ms");
+        tt = tt + (t1-t0);
+        Log.d("TEMPO_TOTAL",(t1-t0)+" ms");
         //Construção do shader
         //Setagem do shader na superficie.
         String vertexShaderSrc = Loader.loadTextFile(context.getResources().openRawResource(R.raw.teste_vertex_shader));
@@ -139,11 +72,7 @@ public class Terrain {
         shader = new GLSLShader(vertexShaderSrc, fragShaderSrc);
         superficie.setShader(shader);
         //tá pronto
-
         alredyBuilt = true;
-        final long t = System.currentTimeMillis();
-        Log.d("TEMPO_GASTO", ""+(t - t0));
-
     }
 
     public Terrain(Bitmap heightmapBmp, Context ctx){
