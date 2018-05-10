a881eca732cfd9454f5a5404300c9486548e850d
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-05-09 21:14:13 -0300

    O bug de ficar preto em algumas direções se repetiu.

--- app/src/main/res/raw/teste_fragment_shader.glsl
+++ app/src/main/res/raw/teste_fragment_shader.glsl
@@ -13,9 +13,16 @@ void main() {
     vec3 worldPosition = ( modelMatrix * vec4(vPosition, 1.0)).xyz;
     //Transformando a normal
     vec3 worldNormal = normalize(vec3(modelMatrix * vec4(vNormal,1.0)));
-    vec3 wnAsColor = (worldNormal + vec3(1,1,1))*0.5;
-    gl_FragColor = vec4( wnAsColor, 1.0 );
+    //vetor da luz pra posição espacial do ponto
+    vec3 lightPosition = lightPositions[0];
+    vec3 lightVector = normalize(lightPosition-worldPosition);
+    //calcula o brilho
+    float brightness = dot(worldNormal, lightVector);
+    //calcula a cor
+    vec3 outputColor = vec3(diffuse * brightness);
 
-
+    gl_FragColor = vec4( outputColor, 1.0 );
+//    vec3 wnAsColor = (worldNormal + vec3(1,1,1))*0.5;
+//    gl_FragColor = vec4( wnAsColor, 1.0 );
 }
 
