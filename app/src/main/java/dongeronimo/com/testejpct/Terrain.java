9f8e165a7afa370baf5a59296b1859cdc4a98436
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-05-11 11:46:28 -0300

    Agora eu acho que realmente estou fazendo o cálculo certo mas estou esbarrando numa diferença entre a quantidade de normais e a quantidade de coordenadas (devido à duplicação)

--- app/src/main/java/dongeronimo/com/testejpct/Terrain.java
+++ app/src/main/java/dongeronimo/com/testejpct/Terrain.java
@@ -3,13 +3,17 @@ package dongeronimo.com.testejpct;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
+import android.util.Pair;
 
 import com.threed.jpct.GLSLShader;
 import com.threed.jpct.Loader;
 import com.threed.jpct.Matrix;
 import com.threed.jpct.Object3D;
 import com.threed.jpct.SimpleVector;
-import com.threed.jpct.TextureManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * O terreno gerado a partir de um heightmap. A partir de um bitmap passado pro construtor constrói
@@ -49,103 +53,63 @@ public class Terrain {
         long tt = 0;
         long t0 = System.currentTimeMillis();
 
-        float coordinates[] = new float[numeroDeTriangulos * 9];
-        float normals[] = new float[numeroDeTriangulos * 9];
-        float uvs[] = new float[numeroDeTriangulos * 6];
-        int indices[] = new int[numeroDeTriangulos * 3];
+        HashMap<Pair<Integer, Integer>, SimpleVector> coordinates = new HashMap<>();//Uma associação entre as coordenadas e suas posições no heigtmap
 
-        int posInCoords = 0;
-        int posInNormals = 0;
         //percorre o heightmap criando as faces.
         for(int y=0; y<bmpAltura-1; y++) {
             for (int x = 0; x < bmpLargura - 1; x++) {
                 //Triangulo 01
                 SimpleVector ponto01 = new SimpleVector(x + worldOffsetX, heightValues[x][y], y + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto01.toArray()[0];
-                coordinates[posInCoords + 1] = ponto01.toArray()[1];
-                coordinates[posInCoords + 2] = ponto01.toArray()[2];
-                posInCoords = posInCoords + 3;
+                coordinates.put(new Pair<>(x,y), ponto01);
                 SimpleVector ponto02 = new SimpleVector(x + worldOffsetX, heightValues[x][y + 1], y + 1 + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto02.toArray()[0];
-                coordinates[posInCoords + 1] = ponto02.toArray()[1];
-                coordinates[posInCoords + 2] = ponto02.toArray()[2];
-                posInCoords = posInCoords + 3;
+                coordinates.put(new Pair<>(x,y+1), ponto02);
                 SimpleVector ponto03 = new SimpleVector(x + 1 + worldOffsetX, heightValues[x + 1][y], y + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto03.toArray()[0];
-                coordinates[posInCoords + 1] = ponto03.toArray()[1];
-                coordinates[posInCoords + 2] = ponto03.toArray()[2];
-                posInCoords = posInCoords + 3;
-                //Calcula as normais desses vertices
-                SimpleVector u0 = ponto02.calcSub(ponto01);
-                SimpleVector v0 = ponto03.calcSub(ponto02);
-                SimpleVector t1Normal = u0.calcCross(v0);
-                normals[posInNormals + 0] = t1Normal.toArray()[0];
-                normals[posInNormals + 1] = t1Normal.toArray()[1];
-                normals[posInNormals + 2] = t1Normal.toArray()[2];
-                posInNormals+=3;
-                normals[posInNormals + 0] = t1Normal.toArray()[0];
-                normals[posInNormals + 1] = t1Normal.toArray()[1];
-                normals[posInNormals + 2] = t1Normal.toArray()[2];
-                posInNormals+=3;
-                normals[posInNormals + 0] = t1Normal.toArray()[0];
-                normals[posInNormals + 1] = t1Normal.toArray()[1];
-                normals[posInNormals + 2] = t1Normal.toArray()[2];
-                posInNormals+=3;
-
+                coordinates.put(new Pair<>(x+1,y), ponto03);
                 //Triangulo 02
                 SimpleVector ponto04 = new SimpleVector(x + worldOffsetX, heightValues[x][y + 1], y + 1 + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto04.toArray()[0];
-                coordinates[posInCoords + 1] = ponto04.toArray()[1];
-                coordinates[posInCoords + 2] = ponto04.toArray()[2];
-                posInCoords = posInCoords + 3;
+                coordinates.put(new Pair<>(x,y+1), ponto04);
                 SimpleVector ponto05 = new SimpleVector(x + 1 + worldOffsetX, heightValues[x + 1][y + 1], y + 1 + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto05.toArray()[0];
-                coordinates[posInCoords + 1] = ponto05.toArray()[1];
-                coordinates[posInCoords + 2] = ponto05.toArray()[2];
-                posInCoords = posInCoords + 3;
+                coordinates.put(new Pair<>(x+1,y+1), ponto05);
                 SimpleVector ponto06 = new SimpleVector(x + 1 + worldOffsetX, heightValues[x + 1][y], y + worldOffsetZ);
-                coordinates[posInCoords + 0] = ponto06.toArray()[0];
-                coordinates[posInCoords + 1] = ponto06.toArray()[1];
-                coordinates[posInCoords + 2] = ponto06.toArray()[2];
-                posInCoords = posInCoords + 3;
-
-                SimpleVector u1 = ponto05.calcSub(ponto04);
-                SimpleVector v1 = ponto06.calcSub(ponto04);
-                SimpleVector t2Normal = u1.calcCross(v1);
-                normals[posInNormals + 0] = t2Normal.toArray()[0];
-                normals[posInNormals + 1] = t2Normal.toArray()[1];
-                normals[posInNormals + 2] = t2Normal.toArray()[2];
-                posInNormals+=3;
-                normals[posInNormals + 0] = t2Normal.toArray()[0];
-                normals[posInNormals + 1] = t2Normal.toArray()[1];
-                normals[posInNormals + 2] = t2Normal.toArray()[2];
-                posInNormals+=3;
-                normals[posInNormals + 0] = t2Normal.toArray()[0];
-                normals[posInNormals + 1] = t2Normal.toArray()[1];
-                normals[posInNormals + 2] = t2Normal.toArray()[2];
-                posInNormals+=3;
-
-                //Adiciona à superficie
-                //superficie.addTriangle(ponto01,0,0, ponto02,0,1, ponto03,1,0);
-                //superficie.addTriangle(ponto04,0,1, ponto05,1,1, ponto06,1,0);
+                coordinates.put(new Pair<>(x+1,y), ponto06);
             }
         }//Com isso a lista de coordenadas está populada
-        for(int i=0; i<uvs.length; i++){
-            uvs[i] = 0.0f;
-        }//Com isso a lista de uvs está populada. No momento não uso a uv pra nada então vou preencher tudo com zero.
-        /** Comentado pra teste.
-        posInNormals = 0;
-        for(int i=0; i<normals.length; i=i+3){
-            normals[posInNormals + 0 ] = 0;
-            normals[posInNormals + 1 ] = 1;
-            normals[posInNormals + 2 ] = 0;
-            posInNormals += 3;
-        }//Normais iguais pra todos nesse teste.
-         */
-        for(int i=0; i<indices.length; i++){
-            indices[i] = i;
-        }//Indices criados
-        superficie = new Object3D(coordinates, normals, uvs, indices, TextureManager.TEXTURE_NOTFOUND);
+        //Elimina coordenadas duplicadas
+        List<SimpleVector> normals = new ArrayList<>();
+        for(int y=0; y<bmpAltura-1; y++) {
+            for (int x = 0; x < bmpLargura - 1; x++) {
+                final Pair<Integer, Integer> currentPos = new Pair<>(x,y);
+                final Pair<Integer, Integer> north = new Pair<>(x, y - 1);
+                final Pair<Integer, Integer> south = new Pair<>(x, y + 1);
+                final Pair<Integer, Integer> east = new Pair<>( x - 1, y);
+                final Pair<Integer, Integer> west = new Pair<>( x+1,y);
+                //lembrando de tratar os casos especiais das bordas - mesma altitude mas afastando na direção apropriada.
+                SimpleVector cCurrent = coordinates.get(currentPos);
+                SimpleVector cNorth = coordinates.get(north);
+                if(cNorth == null)
+                    cNorth = cCurrent.calcAdd(new SimpleVector(0,0,-1));
+                SimpleVector cSouth = coordinates.get(south);
+                if(cSouth == null)
+                    cSouth = cCurrent.calcAdd(new SimpleVector(0,0,1));
+                SimpleVector cEast = coordinates.get(east);
+                if(cEast == null);
+                    cEast = cCurrent.calcAdd(new SimpleVector(-1, 0,0));
+                SimpleVector cWest = coordinates.get(west);
+                if(cWest == null)
+                    cWest = cCurrent.calcAdd(new SimpleVector(1, 0, 0));
+                //Agora eu sei os vetores ao redor do vetor atual. A normal do vetor atual será o produto vetorial
+                //do vetor norte-sul e do vetor leste-oeste
+                final SimpleVector V = cNorth.calcSub(cSouth);
+                final SimpleVector U = cWest.calcSub(cEast);
+                final SimpleVector cross = V.calcCross(U);
+                normals.add(cross);
+            }
+        }
+        int sz0 = coordinates.size();
+        int sz1 = normals.size();
+
+        //Agora cria o objeto 3d com esses dados
+        superficie = new Object3D(1); //(coordinates, normals, uvs, indices, TextureManager.TEXTURE_NOTFOUND);
 
         long t1 = System.currentTimeMillis();
         Log.d("TEMPO_PASSAGEM_GEO", (t1-t0)+" ms");
