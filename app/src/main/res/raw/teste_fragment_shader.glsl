70d3390cb17428dbac2098843d161d4ae6841d27
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-05-13 13:21:33 -0300

    Pela 1a vez consigo distinguir entre as regiões que devem ser de pedreira e as que devem ser de terreno mais plano. A distinção é feita pelo angulo entre a normal do fragmento e a normal de um terreno plano [0,1,0].

--- app/src/main/res/raw/teste_fragment_shader.glsl
+++ app/src/main/res/raw/teste_fragment_shader.glsl
@@ -7,6 +7,12 @@ uniform vec3 cameraPosition;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+float evaluateTerrainByGradient(vec3 fragNormal){
+    vec3 defaultNormal = normalize(vec3(modelMatrix * vec4(vec3(0,1,0),1.0)));
+    float cosBetweenDefaultAndActual = dot(defaultNormal, fragNormal);
+    return cosBetweenDefaultAndActual;
+}
+
 void main() {
     vec3 worldPosition = ( modelMatrix * vec4(vPosition, 1.0)).xyz;
     vec3 worldNormal =  normalize(vec3(modelMatrix * vec4(vNormal,1.0)));
@@ -14,9 +20,16 @@ void main() {
     correctedLightPos.y = -correctedLightPos.y;
     vec3 lightVector = normalize(correctedLightPos-worldPosition);
     float brightness = dot(lightVector, worldNormal);
-    vec3 color = vec3(0.5, 0.5, 0.01);
-    color = color * brightness;
-    gl_FragColor = vec4(color,1);
 
-
+    float normalAngle = evaluateTerrainByGradient(worldNormal);
+    if(normalAngle < 0.5){
+        vec3 color = vec3(0.75, 0.75, 0.75);
+        color = color * brightness;
+        gl_FragColor = vec4(color,1);
+    }
+    else{
+        vec3 color = vec3(0.25, 0.5, 0.01);
+        color = color * brightness;
+        gl_FragColor = vec4(color,1);
+    }
 }
