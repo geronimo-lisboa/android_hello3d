29f439422ccca762716c1ca0244ef6a33d1a17bf
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-05-13 13:49:44 -0300

    Começando a fazer o nivel do mar.

--- app/src/main/res/raw/teste_fragment_shader.glsl
+++ app/src/main/res/raw/teste_fragment_shader.glsl
@@ -4,13 +4,28 @@ uniform mat4 modelMatrix;
 uniform vec3 lightPositions[8];
 uniform vec3 cameraPosition;
 
+uniform float seaLevel;
+
 varying vec3 vNormal;
 varying vec3 vPosition;
-
-float evaluateTerrainByGradient(vec3 fragNormal){
+/*De acordo com o ângulo entre a normal do fragmento e a normal de um terrno totalmente plano (0,1,0)
+eu faço um blending entre a cor de um terreno plano e o terreno de pedreira.*/
+vec3 evaluateTerrainByGradient(vec3 fragNormal){
     vec3 defaultNormal = normalize(vec3(modelMatrix * vec4(vec3(0,1,0),1.0)));
     float cosBetweenDefaultAndActual = dot(defaultNormal, fragNormal);
-    return cosBetweenDefaultAndActual;
+    vec3 pedraColor = vec3(0.75, 0.75, 0.75);
+    vec3 gramaColor = vec3(0.35, 0.45, 0.01);
+
+    return pedraColor * (1.0-cosBetweenDefaultAndActual) + gramaColor * cosBetweenDefaultAndActual;
+}
+
+vec3 evaluateTerrainByHeight(vec3 fragmentPos, vec3 fragmentCurrentColor){
+    if(fragmentPos.y < seaLevel){
+        fragmentCurrentColor.b = 1.0;
+        return normalize(fragmentCurrentColor);
+    }else{
+        return fragmentCurrentColor;
+    }
 }
 
 void main() {
@@ -21,15 +36,8 @@ void main() {
     vec3 lightVector = normalize(correctedLightPos-worldPosition);
     float brightness = dot(lightVector, worldNormal);
 
-    float normalAngle = evaluateTerrainByGradient(worldNormal);
-    if(normalAngle < 0.5){
-        vec3 color = vec3(0.75, 0.75, 0.75);
-        color = color * brightness;
-        gl_FragColor = vec4(color,1);
-    }
-    else{
-        vec3 color = vec3(0.25, 0.5, 0.01);
-        color = color * brightness;
-        gl_FragColor = vec4(color,1);
-    }
+    vec3 terrainColor = evaluateTerrainByGradient(worldNormal);
+    terrainColor = evaluateTerrainByHeight(worldPosition, terrainColor);
+
+    gl_FragColor = vec4(terrainColor * brightness, 1);
 }
